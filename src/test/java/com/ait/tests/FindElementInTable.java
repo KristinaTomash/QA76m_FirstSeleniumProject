026a865c1beb7daecbc4271d000ce57148a4f5c3
KristinaTomash <kristitomsh@gmail.com>
2026-02-23 14:03:12 +0200

    Add less 13

---
+++ src/test/java/com/ait/tests/FindElementInTable.java
@@ -0,0 +1,34 @@
+package com.ait.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FindElementInTable {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://www.w3schools.com/css/css_table.asp");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void findCssSelectorInTable(){
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        if(driver !=null){
+            driver.quit();
+        }
+    }
+}
--- src/test/java/com/ait/tests/FindElementsTests.java
+++ src/test/java/com/ait/tests/FindElementsTests.java
@@ -17,7 +17,7 @@ public class FindElementsTests {
     WebDriver driver;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         driver = new ChromeDriver();
         driver.get("https://icarro-v1.netlify.app/search?page=0&size=10");
 
@@ -27,8 +27,9 @@ public class FindElementsTests {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
     }
+
     @Test
-    public void findElementByTagNameTest(){
+    public void findElementByTagNameTest() {
         //Find ONE element
         //find by tag - h1
         WebElement title = driver.findElement(By.tagName("h1"));
@@ -47,17 +48,86 @@ public class FindElementsTests {
         List<WebElement> labels = driver.findElements(By.tagName("label"));
         System.out.println(labels.size());
 
-
-
         //find list of elements
         List<WebElement> links = driver.findElements(By.tagName("a"));
         System.out.println(links.size());
 
+    }
+
+    @Test
+    public void findElementById() {
+        WebElement city = driver.findElement(By.id("city"));
+        System.out.println(city.getAttribute("id"));
+
+        WebElement dates = driver.findElement(By.id("dates"));
+        System.out.println(dates.getAttribute("id"));
+    }
+
+    @Test
+    public void findElementByClassName() {
+        WebElement telephone = driver.findElement(By.className("telephone"));
+        System.out.println(telephone.getText());
+
+        WebElement description = driver.findElement(By.className("description"));
+        System.out.println(description.getText());
+    }
+    @Test
+    public void findElementByLinkText(){
+        WebElement linkText = driver.findElement(By.linkText("Let car work"));
+        System.out.println(linkText.getText());
 
     }
-    @AfterMethod(enabled = false)
-    public void tearDown(){
-        driver.quit();
+    @Test
+    public void findElementByPartialLinkText(){
+        WebElement partialLink = driver.findElement(By.partialLinkText("work"));
+        System.out.println(partialLink.getText());
+    }
+        @Test
+        public void findElementByCssSelector(){
+        //driver.findElement(By.tagName("h1"));
+            //tag name 'h1'->css 'h1'
+            driver.findElement(By.cssSelector("h1"));
+        //driver.findElement(By.id("city"));
+            //id 'city'-> css '#city'
+            driver.findElement(By.cssSelector("#city"));
+
+            //driver.findElement(By.className("telephone"));
+            //className "telephone" -> css ".telephone"
+            driver.findElement(By.cssSelector(".telephone"));
+
+            // [attr='par']
+            WebElement element = driver.findElement(By.cssSelector("[href='/search']"));
+            System.out.println(element.getText());
+
+            driver.findElement(By.cssSelector("[for='city']"));
+
+            //contains ->*
+            driver.findElement(By.cssSelector("[href*='car']"));
+            //start -> ^
+            driver.findElement(By.cssSelector("[href^='/let']"));
+            //end to -> $
+            driver.findElement(By.cssSelector("[href$='work']"));
+
+            //composite cssSelector(tag+class+pare)
+            driver.findElement(By.cssSelector("a.navigation-link[href='/login']"));
+            //composite cssSelector(tag+class)
+            driver.findElement(By.cssSelector("div.social-networks"));
+            //> one step below
+            driver.findElement(By.cssSelector(".logo>img"));
+            // <space> one or more steps  below ????
+            driver.findElement(By.cssSelector(".feedback-card .feedback-date"));
+            //<tag> or <id> or <class>:nth-child(n)
+            WebElement element1 = driver.findElement(By.cssSelector(".search-container:nth-child(1)"));
+            System.out.println(element1.getText());
+        }
+
+
+    @AfterMethod(enabled = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+
     }
 
 }
